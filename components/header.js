43ca04ebb848c0874d0bd4662bd41f65a69e5b93
kentoda <kent19950811@gmail.com>
2020-12-20 17:21:43 +0900

    header style

--- components/header.js
+++ components/header.js
@@ -5,47 +5,64 @@ import MusicNoteIcon from "@material-ui/icons/MusicNote";
 import RssFeedIcon from "@material-ui/icons/RssFeed";
 import SendIcon from "@material-ui/icons/Send";
 import Image from "next/image";
+import Container from "@material-ui/core/Container";
 
 const Header = () => {
   return (
     <header>
-      <div className="profile">
-        <Image
-          src="/istockphoto-1160498031-612x612.jpg"
-          width={400}
-          height={200}
-        />
-        <h1>Oda Kento</h1>
-      </div>
-      <ul>
-        <Button>
-          <FaceIcon />
-          <Link href="/about">
-            <a>profile</a>
-          </Link>
-        </Button>
+      <Container maxWidth="sm">
+        <div className="profile">
+          <Image
+            className="img"
+            src="/istockphoto-1160498031-612x612.jpg"
+            width={100}
+            height={100}
+          />
+          <h1>Oda Kento</h1>
+        </div>
+        <ul>
+          <Button variant="contained" color="primary" className="button">
+            <FaceIcon />
+            <Link href="/blog">
+              <a>profile</a>
+            </Link>
+          </Button>
 
-        <Button>
-          <RssFeedIcon />
-          <Link href="/about">
-            <a>blog</a>
-          </Link>
-        </Button>
+          <Button variant="contained" color="primary" className="button">
+            <RssFeedIcon />
+            <Link href="/about">
+              <a>blog</a>
+            </Link>
+          </Button>
 
-        <Button>
-          <MusicNoteIcon />
-          <Link href="/about">
-            <a>social</a>
-          </Link>
-        </Button>
+          <Button variant="contained" color="primary" className="button">
+            <MusicNoteIcon />
+            <Link href="/about">
+              <a>social</a>
+            </Link>
+          </Button>
 
-        <Button>
-          <SendIcon />
-          <Link href="/about">
-            <a>message</a>
-          </Link>
-        </Button>
-      </ul>
+          <Button variant="contained" color="primary" className="button">
+            <SendIcon />
+            <Link href="/about">
+              <a>message</a>
+            </Link>
+          </Button>
+        </ul>
+      </Container>
+      <style>{`
+        header .profile {
+          text-align: center;
+        }
+        .img {
+          border-radius: 50%; 
+          object-fit: cover;
+          border: 5px solid #fff
+        }
+        .button {
+          margin-left: 5px;
+        }
+      `}</style>
     </header>
   );
 };
