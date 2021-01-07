5caaf4b5291e009550da78d88979053b7c574ac4
kentoda <kent19950811@gmail.com>
2021-01-07 15:14:26 +0900

    all changes layout

--- pages/blog.js
+++ pages/blog.js
@@ -7,19 +7,16 @@ import Date from "../components/date";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Image from "next/image";
+import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles({
+const madeStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 600,
     margin: 30,
-  },
-  media: {
-    height: 140,
+    marginLeft: 30,
   },
 });
 
@@ -32,51 +29,79 @@ export async function getStaticProps() {
   };
 }
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 750,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    backgroundColor: "#F5F5F5",
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
+
 const Blog = ({ allPostsData }) => {
   const classes = useStyles();
+  const classess = madeStyles();
   return (
-    <div>
-      <Head>
-        <title>blog</title>
-      </Head>
-      <Header />
-      <SubHead />
-
-      <div className="contents">
-        {allPostsData.map(({ id, date, title }) => (
-          <Link href={`/posts/${id}`} key={id}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                {/* <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image="../public/istockphoto-1160498031-612x612.jpg"
-                  title="Contemplative Reptile"
-                /> */}
-                <Image
-                  src="/istockphoto-1160498031-612x612.jpg"
-                  width={500}
-                  height={300}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    <a>{title}</a>
-                  </Typography>
-                  <Typography
-                    // variant="body2"
-                    color="textSecondary"
-                    // component="p"
-                  >
-                    <Date dateString={date} />
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Head>
+            <title>blog</title>
+          </Head>
+          <Header />
+          <SubHead />
+          {/* mapでallPostsDataデータを一つずつ取り出す */}
+          {allPostsData.map(({ id, date, title, img }) => (
+            <Link href={`/posts/${id}`} key={id}>
+              <Card className={classess.root}>
+                <CardActionArea>
+                  <Image src={img} width={500} height={300} />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      <a>{title}</a>
+                    </Typography>
+                    <Typography color="textSecondary">
+                      {/* 日付を文字列に変換表示 */}
+                      <Date dateString={date} />
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
+          ))}
+        </Paper>
+      </main>
+    </>
   );
 };
 
