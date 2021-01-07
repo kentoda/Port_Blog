b69acbc7c667fcfa729598f230e8df31313926b5
kentoda <kent19950811@gmail.com>
2021-01-07 15:11:09 +0900

    change css

--- components/head.js
+++ components/head.js
@@ -2,22 +2,27 @@ const SubHead = () => {
   return (
     <div className="heading">
       <h2>Profile</h2>
+
       <p>エンジニアっぽくないエンジニアになりたい</p>
 
       <style>{`
-         .heading {
+        .heading {
           display: flex;
           align-items: center;
+          margin-left: 30px;
         }
+        
         .heading p {
           color: #66757f;
           margin: 0 0 0 20px;
-      }
+          
+        }
+      
 
-      .heading h2 {
-        border-right: 2px solid #333;
-        padding: 8px 20px 8px 0;
-      }
+        .heading h2 {
+          border-right: 2px solid #333;
+          padding: 8px 20px 8px 0;
+        }
 
       `}</style>
     </div>
